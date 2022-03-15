93fd54c8d9683a58298da03013c6a94df1052ef5
Okin-PC <201130339@student.uj.ac.za>
2022-03-15 22:30:47 +0200

    added a for loop on index.js file

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 const Words = document.querySelector('.words')
-const submitButton = document.querySelector('.btnSubmit')
+const button = document.querySelector('.button')
 const message = document.querySelector('.results')
 
 
 
 function btnClicked() {
-   
-    const Mywords = Words.value;
 
-    if( Mywords.length > 5){
-        message.innerHTML = `sentence too long:  ${Mywords}`;
-        console.log(Mywords)
+    alert("New Number Generated!");
+
+    message.innerHTML = "Error, Number Cannot be smaller than 0";
+
+const Mywords = Words.value;
+const splitword = Mywords.split(" ");
+   for (let i = 0; i < splitword.length; i++) {
+       const element = splitword[i];
+
+       if( element.length > 5){
+          const results = splitword.push(element);
+        message.innerHTML = `sentence too long:  ${results}`;
+        console.log(results)
       }
       
-    else {
-        message.innerHTML = "Type another sentence";
-    }
+    // else {
+    //     message.innerHTML = "Type another sentence";
+    // }
 
+       
+   }
 
-     
-      setTimeout(function(){
-        message.innerHTML = ""
-      }, 3000);
      
  
 }
-submitButton.addEventListener('click', btnClicked)
+button.addEventListener('click', btnClicked)
