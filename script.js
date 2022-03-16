6bc21af718e5906248c80f4f977cf7bb154ac9ac
Okin-PC <201130339@student.uj.ac.za>
2022-03-16 11:53:29 +0200

    Displayed number of words in a sentence index.js file

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 const Words = document.querySelector('.words')
 const button = document.querySelector('.button')
 const message = document.querySelector('.results')
+const NumberOfWords = document.querySelector('.noOfWords')
+
 
 
 
 function btnClicked() {
 
-    alert("Testing");
+
+
+
+//     var str = "your long string with many words.";
+// var wordCount = str.match(/(\w+)/g).length;
+// alert(wordCount); //6
+
+//    \w+    between one and unlimited word characters
+//    /g     greedy - don't stop after the first match
+
+    // alert("Testing");
 
     // message.innerHTML = "Error, Number Cannot be smaller than 0";
 
 const Mywords = Words.value;
 const splitword = Mywords.split(" ");
+
+var wordCount = Mywords.match(/(\w+)/g).length;
+NumberOfWords.innerHTML = wordCount;
+alert(wordCount);
 
 let string = "";
    for (let i = 0; i < splitword.length; i++) {
@@ -21,6 +37,7 @@ let string = "";
         string += "<mark>" + element + "</mark> "
       }
       message.innerHTML = string;
+     
      
       console.log(string)
       
