9cf47f1ca26f8ddb9e9a2d85b94d124c9bf82660
Okin-PC <201130339@student.uj.ac.za>
2022-03-22 10:52:15 +0200

    added an array to keep track of the previous sentences

--- script.js
+++ script.js
@@ -8,11 +8,14 @@ const checkboxes = document.getElementById('checkbox');
 
 
 let prevSentences = [];
+
+
 function btnClicked() {
+
+
 const Mywords = Words.value;
 const splitword = Mywords.split(" ");
 let string = "";
-
 
 var wordCount = Mywords.match(/(\w+)/g).length;
 NumberOfWords.innerHTML = `Number Of Words : ${wordCount}`;
@@ -22,14 +25,20 @@ NumberOfWords.innerHTML = `Number Of Words : ${wordCount}`;
    for (let i = 0; i < splitword.length; i++) {
        const element = splitword[i];
 
+       var aveLength = " ";
+
        if( element.length >= 5){
         string += "<mark>" + element + "</mark> "
+
+        if (element.length > string.length) {
+          console.log(string)
+          aveLength += '<span style="color: orange;">' + element + '</span>'
+        }
        }
       // else if(element.length > 6){
         
       //   string += '<span style="color: orange;">' + element + '</span>'
-      //  }
-      
+      //  }  
       else{
        
      string += element + " ";
@@ -37,11 +46,29 @@ NumberOfWords.innerHTML = `Number Of Words : ${wordCount}`;
   
    }
    prevSentences.push(string)
+
+   if( prevSentences.length <=5){
+    sentences.innerHTML = `Last 5 sentences :  ${prevSentences}`;
+   }
+   
+  //  if it's average word length is higher
+  //  or lower than the last 5 sentences
+  //  green dot if higher and an orange dot if lower
      
    message.innerHTML = `5 Characters Or More :  ${string}`;
-  sentences.innerHTML = `Last 5 sentences :  ${prevSentences}`;
+  
+
+  // if (string.length <= 5) {
+  //   sentences.innerHTML = `Last 5 sentences :  ${prevSentences}`;
+  // }
+
+ 
    
 }
+
+
+
+
 
 
     checkboxes.addEventListener('click', function() {
